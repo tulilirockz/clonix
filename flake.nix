688ff75bd57ef8b2f00d4fc01c33c0028a08a483
Tulili <tulilirockz@outlook.com>
2024-01-11 23:44:59 -0300

    feat: proper formatting with alejandra

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
   description = "Manage rsync deployments declaratively.";
 
-  outputs = _: {
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
     nixosModules = {clonix = import ./modules/nixos.nix;};
     homeManagerModules = {clonix = import ./modules/home-manager.nix;};
+    formatter.${system} = pkgs.alejandra;
   };
 }
